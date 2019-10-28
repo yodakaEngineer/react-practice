9b08936a4f40e3d61589e5ad2d5f5da8f0d104dc
yodaka <engineeryodaka@gmail.com>
2019-10-28 14:46:16 +0900

    make menu.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './reset.css'
+import './App.css'
+import classes from "./App.module.scss";
+import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
+
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const topPage = () => <div><h1>Top Page</h1>ここがトップページです</div>
+    const page1 = () => <div><h1>page1</h1>1枚目のページです</div>
+    const page2 = () => <div><h1>page2</h1>2枚目のページです</div>
+    const page3 = () => <div><h1>page3</h1>3枚目のページです</div>
+    const Menu = () => {
+        return (
+            <ul className={classes.menu}>
+                <li className={classes.menu__link}><Link to='/'>top</Link></li>
+                <li className={classes.menu__link}><Link to='/page1'>page1</Link></li>
+                <li className={classes.menu__link}><Link to='/page2'>page2</Link></li>
+                <li className={classes.menu__link}><Link to='/page3'>page3</Link></li>
+            </ul>
+        )
+    }
+
+    return (
+        <div>
+            <Router>
+                <header className="App-header">
+                    <Menu/>
+                </header>
+
+                <div style={{marginLeft: '50px'}}>
+                    <Switch>
+                        <Route path='/' exact component={topPage}/>
+                        <Route path='/page1' exact component={page1}/>
+                        <Route path='/page2' exact component={page2}/>
+                        <Route path='/page3' exact component={page3}/>
+                        <Route exact component={topPage}/>
+                    </Switch>
+                </div>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
