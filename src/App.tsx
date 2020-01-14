4835aae8a3540106a328bade48040637160b33e4
yodaka <engineeryodaka@gmail.com>
2020-01-14 15:58:33 +0900

    add header.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,20 @@
 import React from 'react';
 import './reset.css'
 import './App.css'
-import classes from "./App.module.scss";
-import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import About from "./pages/About";
+import Header from './components/Header/Header';
 
 const App: React.FC = () => {
     const topPage = () => <div><h1>Top Page</h1>ここがトップページです</div>
     const page2 = () => <div><h1>page2</h1>2枚目のページです</div>
     const page3 = () => <div><h1>page3</h1>3枚目のページです</div>
-    const Menu = () => {
-        return (
-            <ul className={classes.menu}>
-                <li className={classes.menu__link}><Link to='/'>top</Link></li>
-                <li className={classes.menu__link}><Link to='/about'>about</Link></li>
-                <li className={classes.menu__link}><Link to='/page2'>page2</Link></li>
-                <li className={classes.menu__link}><Link to='/page3'>page3</Link></li>
-            </ul>
-        )
-    }
 
     return (
         <div>
             <Router>
                 <header className="App-header">
-                    <Menu/>
+                    <Header/>
                 </header>
 
                 <div style={{marginLeft: '50px'}}>
