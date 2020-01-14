10b4adbe80ed41bc77a1763bb5fd7dea4ddd45d8
yodaka <engineeryodaka@gmail.com>
2020-01-14 15:11:48 +0900

    add About.

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,17 @@ import './reset.css'
 import './App.css'
 import classes from "./App.module.scss";
 import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
-
+import About from "./pages/About";
 
 const App: React.FC = () => {
     const topPage = () => <div><h1>Top Page</h1>ここがトップページです</div>
-    const page1 = () => <div><h1>page1</h1>1枚目のページです</div>
     const page2 = () => <div><h1>page2</h1>2枚目のページです</div>
     const page3 = () => <div><h1>page3</h1>3枚目のページです</div>
     const Menu = () => {
         return (
             <ul className={classes.menu}>
                 <li className={classes.menu__link}><Link to='/'>top</Link></li>
-                <li className={classes.menu__link}><Link to='/page1'>page1</Link></li>
+                <li className={classes.menu__link}><Link to='/about'>about</Link></li>
                 <li className={classes.menu__link}><Link to='/page2'>page2</Link></li>
                 <li className={classes.menu__link}><Link to='/page3'>page3</Link></li>
             </ul>
@@ -31,7 +30,7 @@ const App: React.FC = () => {
                 <div style={{marginLeft: '50px'}}>
                     <Switch>
                         <Route path='/' exact component={topPage}/>
-                        <Route path='/page1' exact component={page1}/>
+                        <Route path='/about' exact component={About}/>
                         <Route path='/page2' exact component={page2}/>
                         <Route path='/page3' exact component={page3}/>
                         <Route exact component={topPage}/>
---
+++ src/pages/About.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const About: React.FC = () => {
+    return (
+        <div><h1>about</h1>Aboutのページです</div>
+    )
+}
+
+export default About
