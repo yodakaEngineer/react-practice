498351b093f2592947cb2d1aa122cf4c02606ac9
yodaka <engineeryodaka@gmail.com>
2020-02-08 15:57:30 +0900

    use reducer.

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useReducer, useState} from "react";
 import Button from '@material-ui/core/Button';
 
 type btnProps = {
@@ -16,26 +16,26 @@ const Btn: React.FC<btnProps> = (props) => {
 }
 
 const Counter: React.FC = () => {
-    const [count, setCount] = useState(0)
-    const countUp = () => {
-        return () => setCount(count + 1)
-    };
-    const countDown = () => {
-        return () => setCount((prevCount)=> {
-            return prevCount === 0 ? prevCount : prevCount - 1
-        })
+    const reduce = (count: number, action: string) => {
+        switch (action) {
+            case 'up':
+                return count + 1
+            case 'down':
+                return count === 0 ? 0 : count - 1
+            case 'reset':
+                return 0
+            default:
+                throw new Error()
+        }
     }
-
-    const reset = () => {
-        return () => setCount(0)
-    }
+    const [count, dispatch] = useReducer(reduce,0)
 
     return (
         <div>
             <p>{count}</p>
-            <Btn name='リセット' onChangeCount={reset()}/>
-            <Btn name='アップ' onChangeCount={countUp()}/>
-            <Btn name='ダウン' onChangeCount={countDown()}/>
+            <Btn name='リセット' onChangeCount={()=>dispatch('reset')}/>
+            <Btn name='アップ' onChangeCount={()=>dispatch('up')}/>
+            <Btn name='ダウン' onChangeCount={()=>dispatch('down')}/>
         </div>
     )
 }
