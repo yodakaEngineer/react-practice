6ea18dbb0aa5e81155c05d6d7d365eef7c4e563e
yodaka <engineeryodaka@gmail.com>
2020-08-04 20:35:45 +0900

    useEffect使うとstateのwatch的なことができるんだー

--- src/components/GetList/GetList.tsx
+++ src/components/GetList/GetList.tsx
@@ -1,18 +1,18 @@
 import {TextField} from "@material-ui/core";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Button from "@material-ui/core/Button";
 import styled from "styled-components";
 import {GetListProps} from "../../pages/List/List";
 
 const GetList: React.FC<GetListProps> = props => {
     const [id,setId] = useState(0)
-    const changeId = (num: number) => {
-        num <= 0 ? setId(0) : setId(num)
-    }
+    useEffect(() => {
+        id <= 0 ? setId(0) : setId(id)
+    }, [id])
 
     return (
         <Wrapper>
-            <TextField id="standard-basic" label="ID検索" type='number' value={id} onChange={(e) => changeId(Number(e.target.value)) } />
+            <TextField id="standard-basic" label="ID検索" type='number' value={id} onChange={(e) => setId(Number(e.target.value)) } />
             <ButtonArea>
                 <Button variant='outlined' onClick={props.onGetList(id)}>findById</Button>
                 <Button variant='outlined' onClick={props.onReset}>reset</Button>
