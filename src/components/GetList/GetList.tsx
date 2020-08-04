abc74bf98f2643f133c288b0064ba00ff50dfb5f
yodaka <engineeryodaka@gmail.com>
2020-08-04 21:12:14 +0900

    resetでIDを0にしたいので、一つ上のコンポーネントに持っていく。

--- src/components/GetList/GetList.tsx
+++ src/components/GetList/GetList.tsx
@@ -1,20 +1,21 @@
 import {TextField} from "@material-ui/core";
-import React, {useEffect, useState} from "react";
+import React from "react";
 import Button from "@material-ui/core/Button";
 import styled from "styled-components";
 import {GetListProps} from "../../pages/List/List";
 
 const GetList: React.FC<GetListProps> = props => {
-    const [id,setId] = useState(0)
-    useEffect(() => {
-        id <= 0 ? setId(0) : setId(id)
-    }, [id])
 
     return (
         <Wrapper>
-            <TextField id="standard-basic" label="ID検索" type='number' value={id} onChange={(e) => setId(Number(e.target.value)) } />
+            <TextField
+                id="standard-basic"
+                label="ID検索"
+                type='number'
+                value={props.id}
+                onChange={(e) => {props.onIdChange(Number(e.target.value))}} />
             <ButtonArea>
-                <Button variant='outlined' onClick={props.onGetList(id)}>findById</Button>
+                <Button variant='outlined' onClick={props.onGetList}>findById</Button>
                 <Button variant='outlined' onClick={props.onReset}>reset</Button>
             </ButtonArea>
         </Wrapper>
